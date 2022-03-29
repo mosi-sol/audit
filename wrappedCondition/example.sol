aad818ddeda0ec6930ab03009568fb7fd4f1dfd9
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-03-29 17:19:53 +0430

    Create example.sol

---
+++ wrappedCondition/example.sol
@@ -0,0 +1,30 @@
+// SPDX-License_identifier: MIT
+pragma solidity 0.8;
+
+contract WrappingConditions {
+    // important: private
+    // SELECTOR = "wrappedTransfer(address,uint256)"  [-> address to, uint value]
+    // data.length == 0  ->[not good impelentation erc20 token like bnb&usdt eth mainnet contracts]
+    // abi.decode(data, (bool))  -> [currect erc20 impelentation]
+    // wrappedTransact  -> is an example, need IERCxxx for work trough this example
+    
+    function wrappedTransact(address token, address to, uint value) private {
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(SELECTOR, to, value)
+        );
+        require(success && (data.length == 0 || abi.decode(data, (bool))),
+            "Transaction failed");
+    }
+
+    // ------------------------------------------
+    // bad coding
+    // sometimes developer doing this type, but dont have 3 importants for doing the job
+    function notWrappedTransact(address token, address to, uint value) private {
+        uint256 amount = value;
+        uint256 balance = token.balanceOf(msg.sender);
+        if (balance < amount) {
+            throw;
+        }
+        token.transfer(to, amount);
+    }
+}
